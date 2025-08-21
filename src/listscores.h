03cb28838a61cd589c688598e1973e9ead65e97d
David Holland <DcHolland_89@msn.com>
2025-08-21 14:46:57 -0400

    Starting to split up functions into its own files to clean up code

---
+++ src/listscores.h
@@ -0,0 +1,16 @@
+#ifndef listscores
+#define listscores
+#pragma once 
+using namespace std;
+using namespace OpenXLSX;
+
+std::vector<int> getScores(const std::string& target_spreadsheet_name, const dpp::slashcommand_t& event);
+#endif // !listscores
+#define listscores
+#pragma once
+#include "main.cpp"
+
+using namespace std;
+using namespace OpenXLSX;
+
+
