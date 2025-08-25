fcfd440cd766ff4645eb79090e7ebba2c06c2f97
David Holland <DcHolland_89@msn.com>
2025-08-25 00:23:32 -0400

    Refactoring code. Separated methods into listscores, updatespreadsheet, and searchmessages. Should be able to maintain and read code better.

--- src/main.h
+++ src/main.h
@@ -8,16 +8,15 @@
 #include <unordered_map>
 #include <vector>
 
-const extern std::string SPREADSHEET_FILENAME;
-const extern std::string TEMP_SPREADSHEET_FILENAME;
-const extern dpp::snowflake GOTCHA_CHANNEL_ID;
-const extern dpp::snowflake OUTPUT_THREAD_ID;
+extern const std::string SPREADSHEET_FILENAME;
+extern const std::string TEMP_SPREADSHEET_FILENAME;
+extern const dpp::snowflake GOTCHA_CHANNEL_ID;
+extern const dpp::snowflake OUTPUT_THREAD_ID;
 extern std::string BOT_TOKEN;
 extern std::mutex spreadsheet_mutex;
 extern std::string CURRENT_SEM;
 
 std::vector<std::string> split(const std::string& s, char delimiter);
-std::pmr::unordered_map<std::string, std::string> createMap();
 std::pmr::unordered_map<std::string, std::string>& getDiscordToName();
 bool starts_with(const std::string& str, const std::string& prefix);
 bool ends_with(const std::string& str, const std::string& suffix);
